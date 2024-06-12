ad194388d463256f224ebaf7c92f7b424e90ae30
videojuegoslagos <160129157+videojuegoslagos@users.noreply.github.com>
2024-06-12 19:06:26 -0300

    Initial commit

---
+++ Assets/Course Library/_Source_Files/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float horizontalInput;
+    float verticalInput;
+    [SerializeField] float speed;
+    public float turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
